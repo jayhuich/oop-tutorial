edba1ef4975b819cdb98641ccae65fd1bc251398
jay <29498758+jayhuich@users.noreply.github.com>
2023-06-12 20:34:59 +0900

    classes, constructors, getters/setters

---
+++ Character.java
@@ -0,0 +1,50 @@
+public class Character {
+
+    private String name;
+    private int health;
+    private int maxHealth;
+
+    // default constructor
+    public Character() {
+        this.name = "Default";
+        this.health = 100;
+        this.maxHealth = 100;
+    }
+
+    // constructor with parameters
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.health = maxHealth;
+        this.maxHealth = maxHealth;
+    }
+
+    // getter function for name
+    public String getName() {
+        return name;
+    }
+
+    // setter function for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public boolean modifyHealth(int amount) {
+        // deny health modification if character is dead
+        if (health <= 0)
+            return false;
+        // prevent overheal
+        if (health + amount > maxHealth)
+            health = maxHealth;
+        else
+            health += amount;
+        return true;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void printCharInfo(Character character) {
+        System.out.println("=== CURRENT STATS ===");
+        System.out.println("Name: " + character.getName());
+        System.out.println("Health: " + character.getHealth() + "/" + character.getMaxHealth());
+        System.out.println("=====================");
+        System.out.println();
+    }
+
+    public static void damage(Character character, int health) {
+        character.modifyHealth(-health);
+        System.out.println("10 damage dealt to " + character.getName() + "!");
+        System.out.println();
+    }
+
+    public static void heal(Character character, int health) {
+        character.modifyHealth(health);
+        System.out.println(character.getName() + " healed 20 HP!");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("World initialized!");
+
+        Character myChar = new Character("daniel", 100);
+        printCharInfo(myChar);
+        damage(myChar, 10);
+        printCharInfo(myChar);
+        heal(myChar, 20);
+        printCharInfo(myChar);
+
+    }
+}
